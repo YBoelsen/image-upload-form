3d6972b3e42d4e283171bc6fc0c9752844cbba5b
YBoelsen <yannick.boelsen@gmail.com>
2022-08-03 19:02:09 +0200

    minor

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
     //   console.log(e.target.files);
     setFile(URL.createObjectURL(e.target.files[0]));
     setContentImg(URL.createObjectURL(e.target.files[0]));
-    console.log(contnetImg)
+    console.log(URL.createObjectURL(e.target.files[0]))
   }
   const styleImageChange = (ele) => {
     console.log(ele.target.value);
