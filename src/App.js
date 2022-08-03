680a5e06dc8a63495b0c1e1921516ac14d14f916
GangCheng0619 <passiondev5555@gmail.com>
2022-08-03 20:04:26 +0800

    fix-image-height

--- src/App.js
+++ src/App.js
@@ -68,8 +68,8 @@ function App() {
             {/* <label className="flex flex-col w-auto p-3 border-4 border-dashed hover:bg-gray-100 hover:border-gray-300"> */}
             {/* <span className='uppercase text-lg text-center text-10xl'>Content Type</span> */}
             <div className='flex justify-center border-grey border-4 p-10'>
-              {file ? <img src={file} className="h-96 w-3/4" width="" id="contentImg" alt="" /> : <svg xmlns="http://www.w3.org/2000/svg"
-                className="w-3/4 h-96 text-gray-400 group-hover:text-gray-600" viewBox="0 0 20 20"
+              {file ? <img src={file} className="h-96" width="" id="contentImg" alt="" /> : <svg xmlns="http://www.w3.org/2000/svg"
+                className=" h-96 text-gray-400 group-hover:text-gray-600" viewBox="0 0 20 20"
                 fill="currentColor">
                 <path fill-rule="evenodd"
                   d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
