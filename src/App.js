a6f675cb8b0ea988878eb2b93ca187d71d2c4e47
GangCheng0619 <passiondev5555@gmail.com>
2022-08-03 17:38:08 +0800

    fixed-height

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import './App.css';
 
 import deepai from 'deepai';
-deepai.setApiKey('89f48157-1d76-497d-aaf5-ced56f21181a');
+// 5f795ad6-5a7b-4a0f-899f-4e44626645ad
+deepai.setApiKey('5f795ad6-5a7b-4a0f-899f-4e44626645ad');
 
-const DIR_URL="http://localhost:3000"; // Need to change this url path.
+const DIR_URL = "http://localhost:3000"; // Need to change this url path.
 // also you need to change ele1~ele7.jpgs in public/assets folder.
 
 const images = [
@@ -22,7 +23,7 @@ function App() {
   const [styleImg, setStyleImg] = useState('');
   const [contnetImg, setContentImg] = useState();
   const handleChange = (e) => {
-  //   console.log(e.target.files);
+    //   console.log(e.target.files);
     setFile(URL.createObjectURL(e.target.files[0]));
     setContentImg(URL.createObjectURL(e.target.files[0]));
   }
@@ -34,8 +35,10 @@ function App() {
     alert("Submit Success!")
     onReset();
     let resp = await deepai.callStandardApi("neural-style", {
-      style: styleImg,
-      content: contnetImg
+      // style: styleImg,
+      // content: contnetImg
+      style: "https://media.istockphoto.com/photos/orange-picture-id185284489",
+      content: "https://media.istockphoto.com/photos/assortment-of-colorful-ripe-tropical-fruits-top-view-picture-id995518546"
     });
     console.log(resp);
   }
@@ -52,21 +55,21 @@ function App() {
             <label for="countries" className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-400">Style Image Types</label>
             <select onChange={styleImageChange} id="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
               <option>Select Style</option>
-              {images.map((ele, ind)=>(<option key={ind} value={ele.URL}>{ele.Name}</option>))}
+              {images.map((ele, ind) => (<option key={ind} value={ele.URL}>{ele.Name}</option>))}
             </select>
           </div>
-          <div className="flex items-center justify-center w-full">
-            <label className="flex flex-col w-3/5 border-4 border-dashed hover:bg-gray-100 hover:border-gray-300">
-              
-              <img src={file} width="100%" height="100%" id="contentImg" />
-              <input type="file" className="opacity-0" onChange={handleChange} />
+          <div className="flex p-2 my-3 space-x-4">
+            <button className="px-4 py-2 text-white bg-green-500 rounded shadow-xl" onClick={onSubmit}>Submit</button>
+            <button className="px-4 py-2 text-white bg-red-500 rounded shadow-xl" onClick={onReset}>Reset</button>
+          </div>
+          <div className="flex-col items-center justify-center w-full h-96">
+            {/* <input type="file" className="opacity-0" id="myfile" onChange={handleChange} /> */}
+            <input type="file" id="myfile" name="myfile" onChange={handleChange} className="mb-2" />
+            <label className="flex flex-col w-full border-4 border-dashed hover:bg-gray-100 hover:border-gray-300">
               <span className='uppercase text-lg text-center'>Content Type</span>
+              <img src={file} className = "h-80" width="100%" height="100%" id="contentImg" />
             </label>
           </div>
-        </div>
-        <div className="flex p-2 space-x-4">
-          <button className="px-4 py-2 text-white bg-green-500 rounded shadow-xl" onClick={onSubmit}>Submit</button>
-          <button className="px-4 py-2 text-white bg-red-500 rounded shadow-xl" onClick={onReset}>Reset</button>
         </div>
       </div>
     </div>
