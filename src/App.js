7a3747d138807741ed461fb260dea8c0bdf8c43c
GangCheng0619 <passiondev5555@gmail.com>
2022-08-03 22:15:22 +0800

    fix-submit

--- src/App.js
+++ src/App.js
@@ -33,14 +33,14 @@ function App() {
   const onSubmit = async () => {
     // alert(`style_image: ${styleImg} \n content_image: ${contnetImg}`);
     alert("Submit Success!")
-    onReset();
+    // onReset();
     let resp = await deepai.callStandardApi("neural-style", {
       // style: styleImg,
       // content: contnetImg
       style: "https://media.istockphoto.com/photos/orange-picture-id185284489",
       content: "https://media.istockphoto.com/photos/assortment-of-colorful-ripe-tropical-fruits-top-view-picture-id995518546"
     });
-    console.log(resp);
+    setFile(resp.output_url);
   }
   const onReset = () => {
     setFile('');
@@ -52,7 +52,7 @@ function App() {
         <div className="m-4">
           <p className="text-center p-2 text-4xl uppercase">Generate Magic Image</p>
           <div className='my-4 flex-col items-center justify-center	'>
-            <label for="countries" className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-400">Style Image Types</label>
+            <label className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-400">Style Image Types</label>
             <select onChange={styleImageChange} id="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
               <option>Select Style</option>
               {images.map((ele, ind) => (<option key={ind} value={ele.URL}>{ele.Name}</option>))}
@@ -71,9 +71,9 @@ function App() {
               {file ? <img src={file} className="h-96" width="" id="contentImg" alt="" /> : <svg xmlns="http://www.w3.org/2000/svg"
                 className=" h-96 text-gray-400 group-hover:text-gray-600" viewBox="0 0 20 20"
                 fill="currentColor">
-                <path fill-rule="evenodd"
+                <path fillRule="evenodd"
                   d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
-                  clip-rule="evenodd" />
+                  clipRule="evenodd" />
               </svg>}
             </div>
 
